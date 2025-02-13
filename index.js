21998c84907bb95b774b24071933d0c88767b328
digvijay <digvijay@g.com>
2025-02-13 21:45:11 +0530

    my

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Savai Sarjacha Changbhala ... ');
+ res.send('Node.js application is running in a container with automatic updates via Jenkins and live deployment!  ... ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
